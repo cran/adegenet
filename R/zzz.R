a9dad5a49a498ed4510c65b2997d4c034d0996bd
Thibaut Jombart <t.jombart@imperial.ac.uk>
2011-12-22 00:00:00 +0000

    version 1.3-4

--- R/zzz.R
+++ R/zzz.R
@@ -2,7 +2,9 @@
 #.initAdegenetClasses()
 #.initAdegenetUtils()
     library.dynam("adegenet", pkg, lib)
-    startup.txt <- "   ==========================\n    adegenet 1.3-3 is loaded\n   ==========================\n\n - to start, type '?adegenet'\n - to browse adegenet website, type 'adegenetWeb()'\n - to post questions/comments: adegenet-forum@lists.r-forge.r-project.org\n\n"
+    pkg.version <- packageDescription("adegenet", fields = "Version")
+
+    startup.txt <- paste("   ==========================\n    adegenet", pkg.version, "is loaded\n   ==========================\n\n - to start, type '?adegenet'\n - to browse adegenet website, type 'adegenetWeb()'\n - to post questions/comments: adegenet-forum@lists.r-forge.r-project.org\n\n")
 
     packageStartupMessage(startup.txt)
 }
